de4a1aa0a83f59a43b1e586ec0a967476eeb5cf3
TheSaviourEking <saviour.eking@outlook.com>
2024-11-30 08:06:20 -0700

    chore: add LogoTicker

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -23,7 +23,7 @@ export default function LogoTicker() {
     return (
         <section className="py-24 overflow-x-clip">
             <div className="container">
-                <h3>Already chosen by this market leaders</h3>
+                <h3 className="text-center text-white/50 text-xl">Already chosen by this market leaders</h3>
                 {/* <div className="overflow-hidden mt-12 [mask-image:linear-gradient(to_right, transparent, black_10%, black_90%,transparent"> */}
                 <div className="overflow-hidden mt-12 mask-image-fade">
                     <div className="flex gap-24 pr-24">
