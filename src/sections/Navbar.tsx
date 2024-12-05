5c0f112240f15fd33c964b9b9a55a4c5c94f4d3e
TheSaviourEking <saviour.eking@outlook.com>
2024-12-04 19:26:33 -0700

    chore: update Navbar animations

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,8 +1,13 @@
+'use client'
+
 import Image from "next/image";
+import { AnimatePresence, motion } from 'framer-motion'
 
 import LogoImage from '@/assets/images/logo.svg'
 import Button from "@/components/Button";
 import Link from "next/link";
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 const navLinks = [
     { label: "Home", href: "#" },
@@ -12,28 +17,60 @@ const navLinks = [
 ];
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <section className="py-4 lg:py-8">
-            <div className="container max-w-5xl">
-                <div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full p-2 items-center px-4 md:pr-2">
-                    <div>
-                        <Image src={LogoImage} alt="Layer Logo" priority className="h-9 md:h-auto w-auto" />
-                    </div>
-                    <div className="hidden lg:flex justify-center items-center">
-                        <nav className="flex gap-6 font-medium">
-                            {navLinks.map(link => (
-                                // <Link href={link.href} key={link.label}>{link.label}</Link>
-                                <a href={link.href} key={link.label}>{link.label}</a>
-                            ))}
-                        </nav>
-                    </div>
-                    <div className="flex justify-end gap-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-menu md:hidden"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
-                        <Button variant='secondary' className="hidden md:block">Login</Button>
-                        <Button variant='primary' className="hidden md:block">Sign up</Button>
+        <>
+            <section className="py-4 lg:py-8 fixed w-full top-0 z-50 ">
+                <div className="container max-w-5xl">
+                    <div className="border border-white/15 rounded-[27px] md:rounded-full bg-neutral-950/70 backdrop-blur">
+                        <div className="grid grid-cols-2 lg:grid-cols-3 p-2 items-center px-4 md:pr-2">
+                            <div>
+                                <Image src={LogoImage} alt="Layer Logo" priority className="h-9 md:h-auto w-auto" />
+                            </div>
+                            <div className="hidden lg:flex justify-center items-center">
+                                <nav className="flex gap-6 font-medium">
+                                    {navLinks.map(link => (
+                                        // <Link href={link.href} key={link.label}>{link.label}</Link>
+                                        <a href={link.href} key={link.label}>{link.label}</a>
+                                    ))}
+                                </nav>
+                            </div>
+                            <div className="flex justify-end gap-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                                    className="feather feather-menu md:hidden"
+                                    onClick={() => setIsOpen(!isOpen)}
+                                >
+                                    <line x1="3" y1="6" x2="21" y2="6" className={twMerge('origin-left transition', isOpen && 'rotate-45 -translate-y-1')}></line>
+                                    <line x1="3" y1="12" x2="21" y2="12" className={twMerge('transition', isOpen && 'opacity-0')}></line>
+                                    <line x1="3" y1="18" x2="21" y2="18" className={twMerge('origin-left transition', isOpen && '-rotate-45 translate-y-1')}></line></svg>
+                                <Button variant='secondary' className="hidden md:block">Login</Button>
+                                <Button variant='primary' className="hidden md:block">Sign up</Button>
+                            </div>
+                        </div>
+
+                        <AnimatePresence>
+                            {isOpen && (
+                                <motion.div
+                                    initial={{ height: 0 }}
+                                    animate={{ height: 'auto' }}
+                                    exit={{ height: 0 }}
+                                    className="overflow-hidden">
+                                    <div className="flex flex-col items-center gap-4 py-4 ">
+                                        {navLinks.map(link => (
+                                            <a href={link.label} key={link.label} className="">{link.label}</a>
+                                        ))}
+
+                                        <Button variant="secondary">Login</Button>
+                                        <Button variant="primary">Login</Button>
+                                    </div>
+                                </motion.div>
+                            )}
+                        </AnimatePresence>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+
+            <div className="pb-[86px] md:pb-[98px] lg:pb-[130px]"></div>
+        </>
     )
 }
